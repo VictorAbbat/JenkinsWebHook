205276587de049257866306e617c5c57f98c5f13
Fred <2095041@bdeb.qc.ca>
2020-12-04 14:31:13 -0500

    origin

--- src/jenkinswebhook/JenkinsWebHook.java
+++ src/jenkinswebhook/JenkinsWebHook.java
@@ -5,6 +5,7 @@ public class JenkinsWebHook {
     
     public static void main(String[] args) {
         System.out.println("Bonjour " + args[1] + "," + args[0]);
+        System.out.println("Ciao Jenkins...!!!");
     }  
     
 }
